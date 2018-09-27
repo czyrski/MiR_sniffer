dabd65ff7d3d005e5bc7c8c0dd58beebad8f9542
czyrski <34021897+czyrski@users.noreply.github.com>
2018-09-27 20:38:09 +0200

    Update MiRSniffer.py

--- MiRSniffer.py
+++ MiRSniffer.py
@@ -1,6 +1,8 @@
 from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager, Screen, CardTransition
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import StringProperty
 from kivy.lang import Builder
+from kivy.clock import Clock
 from os import listdir
 
 kv_path = './kv/'
@@ -12,11 +14,19 @@ class MirSniffer(ScreenManager):
     pass
 
 class MainScreen(Screen):
+    status_mir161 = StringProperty('0')
+
+    def __init__(self, **kwargs):
+        super(MainScreen, self).__init__(**kwargs)
+        Clock.schedule_interval(self.change_robot_status, 5)
+
+    def change_robot_status(self, *args):
+        test_string = self.status_mir161 + '1'
+        self.status_mir161 = test_string
     pass
 
 class Mir161(Screen):
     pass
-
 
 class MainApp(App):
     def build(self):
